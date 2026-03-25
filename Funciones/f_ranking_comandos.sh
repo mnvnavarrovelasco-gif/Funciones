2b236f7ce3d8b3add86d28b8f80738538804b770
mnvnavarrovelasco-gif <mnvnavarrovelasco@gmail.com>
2026-03-25 13:38:40 +0100

    Update f_ranking_comandos.sh

--- Funciones/f_ranking_comandos.sh
+++ Funciones/f_ranking_comandos.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
+#Descripción: Muestra por pantalla los 10 comandos más usados de la shell. 
+#Entrada: Ninguna
+#Salida: Histórico de shell
 f_ranking_comandos() {
 cat ~/.bash_history | awk '{print $1}' | sort | uniq -c | sort -nr | head -10
 return 0
