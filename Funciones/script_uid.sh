d88365595c2e76e9b9e2e5860a7e353a6ce6c17f
mnvnavarrovelasco-gif <mnvnavarrovelasco@gmail.com>
2026-03-25 14:04:56 +0100

    Delete Funciones/script_uid.sh

--- Funciones/script_uid.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#Autor: Brayan De La Rosa
-#Descripción: Este es el script que muestra el UUID del usuario
-#Versión: V1.0.0
-
-echo="Esta es la UUID de este usuario"
-
-sudo blkid
